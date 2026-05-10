45613e285107d8d56fd53985ee1e03665376e4bd
Li-Ting Liao <39208708+ting11222001@users.noreply.github.com>
2026-05-10 23:07:27 +0930

    Define a function with function keyword and it will execute only when it's called later

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -1,3 +1,10 @@
 import { apiKey } from "./util.js";
 
-console.log(apiKey);    
+// console.log(apiKey);  
+
+function greet() {
+    console.log("Hello!");
+}
+
+greet(); // finally executes Hello!
+greet(); // again executes Hello!
